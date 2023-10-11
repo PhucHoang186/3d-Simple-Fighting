23044c1910413d3fce87008d6d7379b241dc59a4
phuc.letruonghoang@gameloft.com <phuck14789@gmail.com>
2023-10-11 17:56:25 +0700

    * fix camera rotate and movement

--- Assets/Scripts/PlayerHandleMovement.cs
+++ Assets/Scripts/PlayerHandleMovement.cs
@@ -8,10 +8,29 @@ public class PlayerHandleMovement : EntityHandleMovement
     [SerializeField] protected float clampXRotationMin;
     [SerializeField] protected float clampXRotationMax;
 
+    public override void Move(Vector3 moveVec)
+    {
+        currentMoveSpeed = Mathf.Lerp(currentMoveSpeed, desMoveSpeed, Time.deltaTime * 5f);
+        if (Physics.Raycast(transform.position, Vector3.forward * moveVec.z, raycastDistance, collideLayer)) // check back and forth
+        {
+            moveVec.z = 0f;
+            moveVec.x *= slowDownPercent;
+        }
+        if (Physics.Raycast(transform.position, Vector3.right * moveVec.x, raycastDistance, collideLayer)) // check left and right
+        {
+            moveVec.x = 0f;
+            moveVec.z *= slowDownPercent;
+        }
+        var newPos = transform.position + moveVec * currentMoveSpeed;
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime);
+    }
+
     public override void Rotate(Vector3 rotateVec)
     {
-        xRotation = Mathf.Lerp(xRotation, xRotation - rotateVec.y * rotateSpeed, Time.deltaTime);
-        yRotation = Mathf.Lerp(yRotation, yRotation + rotateVec.x * rotateSpeed, Time.deltaTime);
+        xRotation -= rotateVec.y * rotateSpeed * Time.deltaTime;
+        yRotation += rotateVec.x * rotateSpeed * Time.deltaTime;
+        // xRotation = Mathf.Lerp(xRotation, xRotation - (rotateVec.y * rotateSpeed), Time.deltaTime * rotateSpeed);
+        // yRotation = Mathf.Lerp(yRotation, yRotation + rotateVec.x * rotateSpeed, Time.deltaTime * rotateSpeed);
         xRotation = Mathf.Clamp(xRotation, clampXRotationMin, clampXRotationMax);
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.localRotation = Quaternion.Euler(0f, yRotation, 0);
